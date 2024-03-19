3080e253441187017c9dc2c1adfc8663b6a2ff09
sergio emiliano <Proyectos_spr@outlook.com>
2024-03-19 01:22:53 -0600

    prueba con ruta ultimo intento

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-
+import django_extensions
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "hello_azure",
 ]
 
--- quickstartproject/urls.py
+++ quickstartproject/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from clases import AgregarRutas
+import django_extensions
 
 
 # Definir las rutas estáticas (por ejemplo, la de administración de Django)
