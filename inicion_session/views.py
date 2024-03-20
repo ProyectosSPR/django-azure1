8c2a28e0893dbbcfa5747c132c80f882f28f3070
sergio emiliano <Proyectos_spr@outlook.com>
2024-03-20 16:06:01 -0600

    correcion 3

--- inicion_session/views.py
+++ inicion_session/views.py
@@ -1,47 +1,43 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-<<<<<<< HEAD
+
 from django.contrib.auth import login
 import logging
 
-logger = logging.getLogger("login_view")
-=======
-from django.http import HttpResponse
-from django.template.context_processors import csrf
->>>>>>> 3fd1a0829ccafd5c619a45a7c9c978541283bf89
 
 @login_required
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
+
+import logging
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+# Crear un logger con el nombre "login_view"
+logger = logging.getLogger("login_view")
+
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
-<<<<<<< HEAD
             logger.info(f"Usuario {user.username} ha iniciado sesión")
-            return redirect("inicio")
+            return redirect("home")
     else:
         form = AuthenticationForm()
     return render(request, "login.html", {"form": form})
-=======
-            return redirect('home')
-        else:
-            return HttpResponse('Invalid login')
-    context = {}
-    context.update(csrf(request))
-    
-    return render(request, 'login.html', context)
->>>>>>> 3fd1a0829ccafd5c619a45a7c9c978541283bf89
+
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
+
 
 @login_required
 def profile_view(request):
-    return render(request, 'profile.html')
+    return render(request, "profile.html")
