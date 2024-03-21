9329c83d802681272b1699e1b342140bb5aa4482
Your Name <Proyectos_spr@outlook.com>
2024-03-20 18:29:16 -0600

    nuevo intento 2

--- inicion_session/views.py
+++ inicion_session/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 import logging
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseForbidden, HttpResponse
 
 
 
@@ -27,15 +28,23 @@ def login_view(request):
             user = form.get_user()
             login(request, user)
             logger.info(f"Usuario {user.username} ha iniciado sesión")
-
             return redirect('home')
         else:
+            # Log si CSRF token no se envía correctamente
+            logger.error("El token CSRF no se envió correctamente en la solicitud.")
             return HttpResponse('Invalid login')
     
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'form': AuthenticationForm()})
+
+
+def handler403(request, exception):
+    # Log el motivo de la denegación
+    logger.error(f"Se denegó la solicitud: {exception}")
+    return HttpResponseForbidden('403 Forbidden')
 
 def logout_view(request):
     logout(request)
+    logger.info("usuario cerro sesion")
     return redirect("login")
 
 
